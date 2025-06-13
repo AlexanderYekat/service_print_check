80277f1f22007b9aa960770b2f87de166f6ee8ee
AlexanderYekat <47818494+AlexanderYekat@users.noreply.github.com>
2025-06-13 16:46:02 +0500

    пытаюсь настроить обновление без ошибок

--- atolservice.php
+++ atolservice.php
@@ -191,6 +191,7 @@ function runServer() {
         // Формируем команду для запуска PowerShell скрипта в фоновом режиме
         $command = "powershell.exe -NoProfile -ExecutionPolicy Bypass -File \"" . $scriptPath . "\" -DownloadUrl " . $updateUrl . " -LogDirPath " . $logPath . " -ServiceNameToStop " . $serviceName . " > NUL 2>&1";
         
+        $logger->info("Команда для запуска PowerShell скрипта: $command");
         pclose(popen($command, 'r'));
         
         $logger->info("Запущено обновление файлов из URL $updateUrl через PowerShell скрипт: $scriptPath");

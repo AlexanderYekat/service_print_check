b9178ae70d859615e46d998b6e162d8c3cde30e2
AlexanderYekat <47818494+AlexanderYekat@users.noreply.github.com>
2025-10-23 21:07:39 +0500

    версию поменял

--- release_to_prod.bat
+++ release_to_prod.bat
@@ -1,18 +1,18 @@
 chcp 65001
 @echo off
-REM Этот скрипт помогает автоматизировать процесс отправки изменений из phprails в prod-phprails.
-REM Он предназначен для использования после того, как вы закончили разработку в phprails и готовы к релизу.
+REM Этот скрипт помогает автоматизировать процесс отправки изменений из async в prod-phprails.
+REM Он предназначен для использования после того, как вы закончили разработку в async и готовы к релизу.
 
-echo [Шаг 1/6] Переключение на ветку phprails...
-git checkout phprails
+echo [Шаг 1/6] Переключение на ветку async...
+git checkout async
 IF %ERRORLEVEL% NEQ 0 (
-    echo Ошибка: Не удалось переключиться на ветку phprails. Проверьте ваш репозиторий.
+    echo Ошибка: Не удалось переключиться на ветку async. Проверьте ваш репозиторий.
     goto :eof
 )
-echo [Шаг 2/6] Обновление phprails с удаленного репозитория...
-git pull origin phprails
+echo [Шаг 2/6] Обновление async с удаленного репозитория...
+git pull origin async
 IF %ERRORLEVEL% NEQ 0 (
-    echo Ошибка: Не удалось обновить phprails. Проверьте ваше сетевое соединение или права доступа.
+    echo Ошибка: Не удалось обновить async. Проверьте ваше сетевое соединение или права доступа.
     goto :eof
 )
 echo [Шаг 3/6] Переключение на ветку prod-phprails...
@@ -27,8 +27,8 @@ IF %ERRORLEVEL% NEQ 0 (
     echo Ошибка: Не удалось обновить prod-phprails. Проверьте ваше сетевое соединение или права доступа.
     goto :eof
 )
-echo [Шаг 5/6] Слияние phprails в prod-phprails...
-git merge phprails
+echo [Шаг 5/6] Слияние async в prod-phprails...
+git merge async
 IF %ERRORLEVEL% NEQ 0 (
     echo Ошибка: Конфликты слияния! Пожалуйста, разрешите их вручную перед повторным запуском скрипта.
     goto :eof
@@ -41,5 +41,5 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 echo.
 echo Процесс завершен. Если не было ошибок, GitHub Action должен быть запущен.
-git checkout phprails
+git checkout async
 pause
